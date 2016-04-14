3be1f5455e3e23e38a775195cbfccb05819f3cdc
Dan Barela <daniel.barela@gmail.com>
2016-04-14 10:32:14 -0600

    removing commented code from a different class

--- test/lib/core/contents/testContents.js
+++ test/lib/core/contents/testContents.js
@@ -25,57 +25,8 @@ describe('Contents tests', function() {
   it('should get the contents', function(done) {
     contentsDao.queryForAll(function(err, contents) {
       console.log('contents', contents);
-      // tileMatrices.should.have.property('length', 4);
       done();
     })
   });
-
-  // it('should transform the tile matrix result to a TileMatrix', function(done) {
-  //   tileMatrixDao.queryForAll(function(err, tileMatrices) {
-  //     tileMatrices[0].should.have.property('table_name', 'TILESosmds');
-  //     tileMatrices[0].should.have.property('zoom_level', 0);
-  //     tileMatrices[0].should.have.property('matrix_width', 1);
-  //     tileMatrices[0].should.have.property('matrix_width', 1);
-  //     tileMatrices[0].should.have.property('tile_height', 256);
-  //     tileMatrices[0].should.have.property('tile_width', 256);
-  //     tileMatrices[0].should.have.property('pixel_x_size', 156543.03392804097);
-  //     tileMatrices[0].should.have.property('pixel_y_size', 156543.033928041);
-  //
-  //     var tileMatrix = tileMatrixDao.createObject();
-  //     tileMatrixDao.populateObjectFromResult(tileMatrix, tileMatrices[0]);
-  //     tileMatrix.should.have.property('matrixWidth', 1);
-  //     tileMatrix.should.have.property('matrixHeight', 1);
-  //     tileMatrix.should.have.property('zoomLevel', 0);
-  //     tileMatrix.should.have.property('tableName', 'TILESosmds');
-  //     tileMatrix.should.have.property('tileWidth', 256);
-  //     tileMatrix.should.have.property('tileHeight', 256);
-  //     tileMatrix.should.have.property('pixelXSize', 156543.03392804097);
-  //     tileMatrix.should.have.property('pixelYSize', 156543.033928041);
-  //
-  //     done();
-  //   });
-  // });
-  //
-  // // TODO find a geopackage that has Contents
-  // it('should get the Contents from a TileMatrix', function(done) {
-  //   tileMatrixDao.queryForAll(function(err, tileMatrices) {
-  //     tileMatrixDao.getContents(tileMatrices[0], function(err, contents) {
-  //       console.log('err', err);
-  //       console.log('contents', contents);
-  //       done();
-  //     });
-  //   });
-  // });
-  //
-  // // TODO find a geopackage that has TileMatrixSet
-  // it('should get the TileMatrixSet from a TileMatrix', function(done) {
-  //   tileMatrixDao.queryForAll(function(err, tileMatrices) {
-  //     tileMatrixDao.getTileMatrixSet(tileMatrices[0], function(err, tileMatrixSet) {
-  //       console.log('err', err);
-  //       console.log('tileMatrixSet', tileMatrixSet);
-  //       done();
-  //     });
-  //   });
-  // });
 
 });
