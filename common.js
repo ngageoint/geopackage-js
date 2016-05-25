14daf0f7d1cc899b9c4f1e930cd7faf3b54c1503
Dan Barela <daniel.barela@gmail.com>
2016-05-25 10:52:50 -0600

    adding SRS to the contents section and correcting bugs with tile positioning

--- common.js
+++ common.js
@@ -48,7 +48,10 @@ var GeoPackage = GeoPackageAPI.GeoPackage
   baseLayer.addTo(map);
 
   var geoPackage;
-  var tableLayers = {};
+  var tableLayers;
+  var imageOverlay;
+  var currentTile = {};
+  var tableInfos;
 
   window.loadGeoPackage = function(files) {
     var f = files[0];
@@ -61,8 +64,6 @@ var GeoPackage = GeoPackageAPI.GeoPackage
     r.readAsArrayBuffer(f);
   }
 
-  var tableInfos;
-
   function loadByteArray(array, callback) {
     tableInfos = {};
     var tileTableNode = $('#tile-tables');
@@ -72,6 +73,10 @@ var GeoPackage = GeoPackageAPI.GeoPackage
 
     for (layerName in tableLayers) {
       map.removeLayer(tableLayers[layerName]);
+    }
+    tableLayers = {};
+    if (imageOverlay) {
+      map.removeLayer(imageOverlay);
     }
 
     var featureTableTemplate = $('#feature-table-template').html();
@@ -346,9 +351,6 @@ var GeoPackage = GeoPackageAPI.GeoPackage
       });
     });
   }
-
-  var imageOverlay;
-  var currentTile = {};
 
   window.zoomToTile = function(tileColumn, tileRow, zoom, minLongitude, minLatitude, maxLongitude, maxLatitude, projection, tableName) {
     if (imageOverlay) map.removeLayer(imageOverlay);
