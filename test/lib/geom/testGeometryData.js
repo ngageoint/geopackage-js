1fae2762cde0f18d20f951431b5f1e470a3169ab
Dan Barela <daniel.barela@gmail.com>
2016-09-01 07:45:21 -0600

    fixed a bug where empty was not being set to false

--- test/lib/geom/testGeometryData.js
+++ test/lib/geom/testGeometryData.js
@@ -244,10 +244,12 @@ describe('Geometry Data tests', function() {
     done();
   });
 
-  it('should create a point geometry data with an envelope', function(done) {
+  it.only('should create a point geometry data with an envelope', function(done) {
     var geometryData = new GeometryData();
+    geometryData.empty.should.be.equal(true);
     geometryData.setSrsId(4326);
     geometryData.setGeometry(rawPoint);
+    geometryData.empty.should.be.equal(false);
     geometryData.setEnvelope({
       minX: 5.0,
       maxX: 78.0,
