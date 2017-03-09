ddb0aa545a79dbfc0031a58e0c376487ae8e0ba3
Dan Barela <daniel.barela@gmail.com>
2017-03-09 11:10:15 -0700

    properly add the attribution credit

--- docs/leaflet/index.js
+++ docs/leaflet/index.js
@@ -1,7 +1,9 @@
 
 var map = L.map('map').setView([45, 15], 3);
 
-var baseLayer = L.tileLayer('https://mapbox.geointservices.io/v4/mapbox.light/{z}/{x}/{y}.png');
+var baseLayer = L.tileLayer('https://mapbox.geointservices.io/v4/mapbox.light/{z}/{x}/{y}.png', {
+  attribution: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
+});
 baseLayer.addTo(map);
 
 L.gridLayer.geoPackage({
