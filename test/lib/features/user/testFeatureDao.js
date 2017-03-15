2fef445cd3769040238abec8cd73dbe4c46cc1b4
Dan Barela <daniel.barela@gmail.com>
2017-03-15 11:16:33 -0600

    upgrading browserify to 14 and adding new tests

--- test/lib/features/user/testFeatureDao.js
+++ test/lib/features/user/testFeatureDao.js
@@ -1,20 +1,34 @@
-// var FeatureDao = require('../../../../lib/features/user/featureDao')
-//   , GeoPackageConnection = require('../../../../lib/db/geoPackageConnection')
-//   , path = require('path')
-//   , should = require('chai').should();
-//
-// describe.only('Feature Dao tests', function() {
-//
-//   beforeEach('create the GeoPackage connection', function(done) {
-//     GeoPackageConnection.connect(path.join(__dirname, '..', '..', '..', 'fixtures', 'rivers.gpkg'), function(err, geoPackageConnection) {
-//       connection = geoPackageConnection;
-//       should.exist(connection);
-//       done();
-//     });
-//   });
-//
-//   it('should test', function(done) {
-//     done();
-//   });
-//
-// });
+var FeatureDao = require('../../../../lib/features/user/featureDao.js')
+  , GeoPackageManager = require('../../../../lib/geoPackageManager.js')
+  , path = require('path')
+  , should = require('chai').should();
+
+describe('FeatureDao tests', function() {
+
+  var geoPackage;
+  beforeEach('create the GeoPackage connection', function(done) {
+    var filename = path.join(__dirname, '..', '..', '..', 'fixtures', 'rivers.gpkg');
+    GeoPackageManager.open(filename, function(err, gp) {
+      geoPackage = gp;
+      done();
+    });
+  });
+
+  afterEach('close the geopackage connection', function() {
+    geoPackage.close();
+  });
+
+  it('should read the geometry', function(done) {
+    geoPackage.getFeatureDaoWithTableName('FEATURESriversds', function(err, featureDao) {
+      featureDao.getSrs(function(err, srs) {
+        featureDao.queryForEach(function(err, row, rowDone) {
+          var currentRow = featureDao.getFeatureRow(row);
+          var geometry = currentRow.getGeometry();
+          should.exist(geometry);
+          done();
+        });
+      });
+    });
+  });
+
+});
