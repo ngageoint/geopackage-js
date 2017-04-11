91c258d22bf751691032da8b8f8c9a7265bb52c3
Dan Barela <daniel.barela@gmail.com>
2017-04-11 08:49:43 -0600

    updating to osm tiles

--- docs/common.js
+++ docs/common.js
@@ -33,7 +33,7 @@ var map = L.map('map', {
 
 map.addControl(new L.Control.ZoomIndicator());
 
-var baseLayer = L.tileLayer('https://mapbox.geointservices.io/v4/mapbox.light/{z}/{x}/{y}.png', {
+var baseLayer = L.tileLayer('https://osm.geointservices.io/tiles/{z}/{x}/{y}.png', {
   attribution: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
 });
 baseLayer.addTo(map);
--- docs/leaflet/index.js
+++ docs/leaflet/index.js
@@ -2,46 +2,46 @@ require('leaflet-geopackage');
 
 var map = L.map('map').setView([45, 15], 3);
 
-var baseLayer = L.tileLayer('https://mapbox.geointservices.io/v4/mapbox.light/{z}/{x}/{y}.png', {
+var baseLayer = L.tileLayer('https://osm.geointservices.io/tiles/{z}/{x}/{y}.png', {
   attribution: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
 });
 baseLayer.addTo(map);
 
-var tileLayer = L.tileLayer('http://localhost:4234/rivers_indexed/rivers/{z}/{x}/{y}.png', {
-  attribution: 'GeoPackage JS'
-});
-
-tileLayer.addTo(map);
-
-var tileLayer2 = L.tileLayer('http://localhost:4234/rivers_indexed/rivers_tiles/{z}/{x}/{y}.png', {
-  attribution: 'GeoPackage JS'
-});
+// var tileLayer = L.tileLayer('http://localhost:4234/rivers_indexed/rivers/{z}/{x}/{y}.png', {
+//   attribution: 'GeoPackage JS'
+// });
+//
+// tileLayer.addTo(map);
+//
+// var tileLayer2 = L.tileLayer('http://localhost:4234/rivers_indexed/rivers_tiles/{z}/{x}/{y}.png', {
+//   attribution: 'GeoPackage JS'
+// });
 
 //tileLayer2.addTo(map);
 
-// var tileLayer = L.geoPackageTileLayer({
-//     geoPackageUrl: 'http://ngageoint.github.io/GeoPackage/examples/rivers.gpkg',
-//     layerName: 'rivers_tiles'
-// }).addTo(map);
-//
-// tileLayer.on('load', function() {
-//   tileLayer.off('load');
-//   L.geoPackageFeatureLayer([], {
-//       geoPackageUrl: 'http://ngageoint.github.io/GeoPackage/examples/rivers.gpkg',
-//       layerName: 'rivers',
-//       style: function (feature) {
-//         return {
-//           color: "#F00",
-//           weight: 2,
-//           opacity: 1
-//         };
-//       },
-//       onEachFeature: function (feature, layer) {
-//         var string = "";
-//         for (var key in feature.properties) {
-//           string += '<div class="item"><span class="label">' + key + ': </span><span class="value">' + feature.properties[key] + '</span></div>';
-//         }
-//         layer.bindPopup(string);
-//       }
-//   }).addTo(map);
-// });
+var tileLayer = L.geoPackageTileLayer({
+    geoPackageUrl: 'http://ngageoint.github.io/GeoPackage/examples/rivers.gpkg',
+    layerName: 'rivers_tiles'
+}).addTo(map);
+
+tileLayer.on('load', function() {
+  tileLayer.off('load');
+  L.geoPackageFeatureLayer([], {
+      geoPackageUrl: 'http://ngageoint.github.io/GeoPackage/examples/rivers.gpkg',
+      layerName: 'rivers',
+      style: function (feature) {
+        return {
+          color: "#F00",
+          weight: 2,
+          opacity: 1
+        };
+      },
+      onEachFeature: function (feature, layer) {
+        var string = "";
+        for (var key in feature.properties) {
+          string += '<div class="item"><span class="label">' + key + ': </span><span class="value">' + feature.properties[key] + '</span></div>';
+        }
+        layer.bindPopup(string);
+      }
+  }).addTo(map);
+});
