d5e98889cdb9a21581554e471d3d9bb0e696f2ab
Dan Barela <daniel.barela@gmail.com>
2017-04-12 15:07:33 -0600

    moving from lwip 0.0.9 to pajk-lwip 0.2.0 for node 6.10.2

--- test/lib/issues/issue68.js
+++ test/lib/issues/issue68.js
@@ -4,13 +4,12 @@ var GeoPackage = require('../../../lib/geoPackage')
 
 var path = require('path')
   , should = require('chai').should();
-// , proj4 = require('proj4');
 
 describe('Tests for issue 68', function() {
 
   it('should get a tile', function(done) {
 
-    // this.timeout(5000);
+    this.timeout(5000);
     GeoPackageConnection.connect(path.join(__dirname, '..', '..', 'fixtures', 'issue_68.gpkg'), function(err, connection) {
       var geoPackage = new GeoPackage('', '', connection);
        geoPackage.getTileTables(function(err, tables) {
@@ -21,15 +20,14 @@ describe('Tests for issue 68', function() {
           geoPackage.getInfoForTable(tileDao, function(err, info) {
             if (err) return done(err);
             should.exist(info);
-            done();
-            // var gpr = new GeoPackageTileRetriever(tileDao, 256, 256);
-            // gpr.getTile(192,401,10,function(err, tile) {
-    //           if (err) return done(err);
-    //           should.exist(tile);
-    //           delete tile;
-    //           geoPackage.close();
-              // done();
-            // });
+            var gpr = new GeoPackageTileRetriever(tileDao, 256, 256);
+            gpr.getTile(192,401,10,function(err, tile) {
+              if (err) return done(err);
+              should.exist(tile);
+              delete tile;
+              geoPackage.close();
+              done();
+            });
           });
         });
       });
