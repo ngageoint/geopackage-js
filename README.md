5ed9bbc0854a9449b9c9462f7e9299ce95e0723f
Dan Barela <daniel.barela@gmail.com>
2017-08-10 08:09:28 -0600

    FeatureDao.queryIndexedFeaturesWithWebMercatorBoundingBox now returns a complete feature row fixes #92

--- README.md
+++ README.md
@@ -33,6 +33,10 @@ The GeoPackage JavaScript library currently provides the ability to read GeoPack
 
 ### Changelog
 
+##### 1.0.22
+
+- Fixed bug where querying for indexed features only returned the geometry instead of the entire feature
+
 ##### 1.0.19
 
 - Remove dependency on Lwip
--- test/lib/features/user/testFeatureDao.js
+++ test/lib/features/user/testFeatureDao.js
@@ -1,32 +1,97 @@
 var FeatureDao = require('../../../../lib/features/user/featureDao.js')
   , GeoPackageManager = require('../../../../lib/geoPackageManager.js')
+  , BoundingBox = require('../../../../lib/boundingBox.js')
+  , fs = require('fs')
   , path = require('path')
   , should = require('chai').should();
 
 describe('FeatureDao tests', function() {
 
-  var geoPackage;
-  beforeEach('create the GeoPackage connection', function(done) {
-    var filename = path.join(__dirname, '..', '..', '..', 'fixtures', 'rivers.gpkg');
-    GeoPackageManager.open(filename, function(err, gp) {
-      geoPackage = gp;
-      done();
+  describe('Non indexed test', function() {
+    var geoPackage;
+    beforeEach('create the GeoPackage connection', function(done) {
+      var filename = path.join(__dirname, '..', '..', '..', 'fixtures', 'rivers.gpkg');
+      GeoPackageManager.open(filename, function(err, gp) {
+        geoPackage = gp;
+        done();
+      });
+    });
+
+    afterEach('close the geopackage connection', function() {
+      geoPackage.close();
+    });
+
+    it('should read the geometry', function(done) {
+      geoPackage.getFeatureDaoWithTableName('FEATURESriversds', function(err, featureDao) {
+        featureDao.getSrs(function(err, srs) {
+          featureDao.queryForEach(function(err, row, rowDone) {
+            var currentRow = featureDao.getFeatureRow(row);
+            var geometry = currentRow.getGeometry();
+            should.exist(geometry);
+            rowDone();
+          }, done);
+        });
+      });
     });
   });
 
-  afterEach('close the geopackage connection', function() {
-    geoPackage.close();
-  });
+  describe('Indexed test', function() {
+    var geoPackage;
+    var featureDao;
 
-  it('should read the geometry', function(done) {
-    geoPackage.getFeatureDaoWithTableName('FEATURESriversds', function(err, featureDao) {
-      featureDao.getSrs(function(err, srs) {
-        featureDao.queryForEach(function(err, row, rowDone) {
-          var currentRow = featureDao.getFeatureRow(row);
-          var geometry = currentRow.getGeometry();
-          should.exist(geometry);
-          rowDone();
-        }, done);
+    var originalFilename = path.join(__dirname, '..', '..', '..', 'fixtures', 'rivers_indexed.gpkg');
+    var filename = path.join(__dirname, '..', '..', '..', 'fixtures', 'tmp', 'rivers_indexed.gpkg');
+
+    function copyGeopackage(orignal, copy, callback) {
+      if (typeof(process) !== 'undefined' && process.version) {
+        var fsExtra = require('fs-extra');
+        fsExtra.copy(originalFilename, filename, callback);
+      } else {
+        filename = originalFilename;
+        callback();
+      }
+    }
+
+    beforeEach('should open the geopackage', function(done) {
+      copyGeopackage(originalFilename, filename, function(err) {
+        GeoPackageManager.open(filename, function(err, gp) {
+          geoPackage = gp;
+          should.not.exist(err);
+          should.exist(gp);
+          should.exist(gp.getDatabase().getDBConnection());
+          gp.getPath().should.be.equal(filename);
+          geoPackage.getFeatureDaoWithTableName('rivers', function(err, riverFeatureDao) {
+            featureDao = riverFeatureDao;
+            done();
+          });
+        });
+      });
+    });
+
+    afterEach('should close the geopackage', function(done) {
+      geoPackage.close();
+      if (typeof(process) !== 'undefined' && process.version) {
+        fs.unlink(filename, done);
+      } else {
+        done();
+      }
+    });
+
+    it('should query for indexed geometries', function(done) {
+      var count = 0;
+      var bbox = new BoundingBox(-12863648.645994272, -12865751.85860068, 6655573.571054254, 6651886.678768059);
+      featureDao.queryIndexedFeaturesWithWebMercatorBoundingBox(bbox, function(err, featureRow, rowCallback) {
+        console.log('featureRow', featureRow);
+        should.exist(featureRow.getValueWithColumnName('geom'));
+        should.exist(featureRow.getValueWithColumnName('id'));
+        should.exist(featureRow.getValueWithColumnName('property_0'));
+        should.exist(featureRow.getValueWithColumnName('property_1'));
+        should.exist(featureRow.getValueWithColumnName('property_2'));
+        count++;
+        rowCallback();
+      }, function(err) {
+        console.log('count', count);
+        done();
       });
     });
   });
