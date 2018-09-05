45bfcd3adac4551cc92e6c6b2c02640168497c46
Dan Barela <daniel.barela@gmail.com>
2018-09-05 15:53:22 -0600

    leaflet demo 4326

--- docs/leaflet/index.js
+++ docs/leaflet/index.js
@@ -1,13 +1,15 @@
-var map = L.map('map').setView([45, 15], 3);
+var map = L.map('map', {
+  crs: L.CRS.EPSG4326
+}).setView([45, 15], 3);
 
-var baseLayer = L.tileLayer('https://osm.geointservices.io/tiles/{z}/{x}/{y}.png', {
-  attribution: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
+var baseLayer = L.tileLayer('https://osm.geointservices.io/tiles/default_pc/{z}/{x}/{y}.png', {
+  attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 });
 baseLayer.addTo(map);
 
 var tileLayer = L.geoPackageTileLayer({
-    geoPackageUrl: 'https://ngageoint.github.io/GeoPackage/examples/rivers.gpkg',
-    layerName: 'rivers_tiles'
+    geoPackageUrl: './4326.gpkg',
+    layerName: 'Slate_Canvas_tiles'
 }).addTo(map);
 
 tileLayer.on('load', function() {
