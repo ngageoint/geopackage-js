1df2fbfeb18369926b8b3b3f0cf75ed3547031db
Dan Barela <daniel.barela@gmail.com>
2018-10-09 12:42:44 -0600

    filter out __MACOSX files

--- docs/index.js
+++ docs/index.js
@@ -26,6 +26,6 @@ require('leaflet.vectorgrid/dist/Leaflet.VectorGrid.bundled.js');
 
 window.GeoPackageAPI = require('@ngageoint/geopackage');
 // window.GeoJSONToGeoPackage = require('geojson-to-geopackage');
-// window.ShapefileToGeoPackage = require('shapefile-to-geopackage');
+window.ShapefileToGeoPackage = require('@ngageoint/shapefile-to-geopackage');
 // window.MBTilesToGeoPackage = require('mbtiles-to-geopackage');
 // window.PBFToGeoPackage = require('pbf-to-geopackage');
