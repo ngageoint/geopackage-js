b531cd8e93279c1c0357751505b9581ff4769da8
Dan Barela <daniel.barela@gmail.com>
2018-10-09 09:40:34 -0600

    Vector tile calls in the API return promises properly escape table names with hyphens when querying for indexed features

--- test/lib/features/user/testFeatureDao.js
+++ test/lib/features/user/testFeatureDao.js
@@ -106,7 +106,6 @@ describe('FeatureDao tests', function() {
       var count = 0;
       var bbox = new BoundingBox(-13189576.119, -13126488.564, 6637372.21, 6607360.178);
       var iterator = featureDao.queryIndexedFeaturesWithWebMercatorBoundingBox(bbox);
-
       for (var row of iterator) {
         count++;
         row.values.property_1.should.be.equal('Columbia');
