d2826502ce75bb1acb9e9456f3afd0b8c45b1008
Robert St. John <restjohn@users.noreply.github.com>
2024-02-26 17:26:55 -0700

    comment on future change

--- lib/db/dbAdapter.ts
+++ lib/db/dbAdapter.ts
@@ -1,3 +1,8 @@
+/**
+ * TODO:
+ * This should allow `null` as well, but adding that could be a breaking change to clients.
+ * Additionally set `"strictNullChecks": true` in tsconfig.
+ */
 export type DBValue = boolean | string | number | Buffer | Uint8Array;
 
 export interface DBAdapter {
